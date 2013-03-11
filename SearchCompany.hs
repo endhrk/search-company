c83ff44ac34daed3ece5a914e531ac71a8f8e635
endhrk <endhrk@gmail.com>
2013-03-11 14:29:16 +0900

    cabalのGHCオプションで警告は全て出すように変更。出ていた警告を修正。

--- SearchCompany.hs
+++ SearchCompany.hs
@@ -4,21 +4,25 @@ import System.Environment
 import Codec.Text.IConv (convert)
 import Data.ByteString.Lazy.Char8 (pack,unpack)
 import Codec.Binary.UTF8.String (encodeString,decodeString)
-import Control.Exception (evaluate)
+import Control.Exception
 import Text.Regex.TDFA
 import Control.Exception as CE
+import System.Info as SYS
 
 convertCode :: String -> String -> String
-convertCode code src = decodeString $ unpack $ convert code "UTF-8" $ pack $ encodeString src
+convertCode code src = decodeString $ unpack $ convert code (getOSCharCode) $ pack $ encodeString src
+
+getOSCharCode :: String
+getOSCharCode = if (SYS.os == "mingw32") then "Shift-JIS" else "UTF-8"
 
 getWhoisServer :: String -> String
-getWhoisServer ip = "192.41.192.40"
+getWhoisServer _ = "192.41.192.40"
 
 getWhoisCharCode :: String -> String
-getWhoisCharCode ip = "ISO-2022-JP"
+getWhoisCharCode _ = "ISO-2022-JP"
 
 getCompanyRegex :: String -> String
-getCompanyRegex ip = "^f."
+getCompanyRegex _ = "^f."
 
 searchCompanyFromIp :: String -> IO String
 searchCompanyFromIp ip = do
@@ -53,14 +57,17 @@ dispatch "--csv" fileName = do
     cs <- CE.catch (readFile fileName) (catchException fileName)
     companies <- mapM searchCompanyFromIp $ lines cs
     mapM_ putStrLn $ zipWith makeCsv (lines cs) companies
+dispatch _ _ = do usage
 
 catchException :: String -> IOError -> IO String
-catchException filename error = do
+catchException filename _ = do
     hPutStrLn stderr $ "File not found: " ++ filename
     return []
 
+usage :: IO ()
 usage = putStrLn "usage: searchCompany [--ip IP_ADDR|--csv FILE_NAME]"
 
+main :: IO ()
 main = do
     args <- getArgs
     if length args /= 2
