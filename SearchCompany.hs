eeefd917029919b3174534f97b7b27ef5bb4984c
endhrk <endhrk@gmail.com>
2013-03-22 11:24:27 +0900

    whoisの切り出しとリファクタリング

--- SearchCompany.hs
+++ SearchCompany.hs
@@ -3,6 +3,13 @@ import System.Environment
 import Text.Regex.TDFA
 import Control.Exception as CE
 import Network.Whois
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Text.Encoding
+import qualified Data.Text
+import Data.IP
+import Data.Maybe
+import System.Console.ParseArgs
+import qualified Control.Applicative as CA
 
 getCompanyRegex :: String -> String
 getCompanyRegex _ = "^f."
@@ -10,7 +17,7 @@ getCompanyRegex _ = "^f."
 searchCompanyFromIp :: String -> IO String
 searchCompanyFromIp ip = do
     contents <- whois ip
-    return . getCompanyName . unlines . filter (isCompany (getCompanyRegex ip)) . lines $ contents
+    return . getCompanyName . unlines . filter (isCompany (getCompanyRegex ip)) . lines $ Data.Text.unpack $ Data.Text.Encoding.decodeUtf8 contents
 
 isCompany :: String -> String -> Bool
 isCompany regex line = line =~ regex :: Bool
@@ -21,29 +28,29 @@ getCompanyName line = line =~ "([^ ]*[ |.][^ ]+)$" :: String
 makeCsv :: String -> String -> String
 makeCsv a b = a ++ "," ++ b 
 
-dispatch :: String -> String -> IO ()
-dispatch "--ip" ip = do
-    companyName <- searchCompanyFromIp ip
-    putStrLn $ makeCsv ip companyName
-dispatch "--csv" fileName = do
-    cs <- CE.catch (readFile fileName) (catchException fileName)
-    companies <- mapM searchCompanyFromIp $ lines cs
-    mapM_ putStrLn $ zipWith makeCsv (lines cs) companies
-dispatch _ _ = do usage
-
-catchException :: String -> IOError -> IO String
-catchException filename _ = do
-    hPutStrLn stderr $ "File not found: " ++ filename
-    return []
-
-usage :: IO ()
-usage = putStrLn "usage: searchCompany [--ip IP_ADDR|--csv FILE_NAME]"
+options :: [Arg String]
+options =
+    [ Arg "csv" (Just 'c') (Just "csv") (argDataOptional "FILE_PATH" ArgtypeString) "input csv file path"
+    , Arg "out" (Just 'o') (Just "output") (argDataDefaulted "FILE_PATH" ArgtypeString "output.csv") "output file path"
+    , Arg "ip" Nothing Nothing (argDataOptional "IP_ADDRESS" ArgtypeString) "ip address"
+    ]
 
 main :: IO ()
 main = do
-    args <- getArgs
-    if length args /= 2
-        then usage
-        else do
-             (command:ip:etc) <- return args
-             dispatch command ip
+    a <- parseArgsIO ArgsComplete options
+    output <- fromJust CA.<$> getArgFile a "out" WriteMode
+    if gotArg a "csv"
+    then do
+        input <- getArgFile a "csv" ReadMode
+        contents <- hGetContents $ fromJust input
+        companies <- mapM searchCompanyFromIp $ lines contents
+        mapM_ (hPutStrLn output) $ zipWith makeCsv (lines contents) companies
+        hClose output
+    else if gotArg a "ip"
+    then do
+        let ip = fromJust $ getArgString a "ip"
+        companyName <- searchCompanyFromIp ip
+        hPutStrLn output (makeCsv ip companyName)
+        hClose output
+    else do
+        putStrLn $ argsUsage a
